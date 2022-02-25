ff4fa92129428d7fca68d93f520cc9ae24826301
Gabriel <gabriel.costa513@gmail.com>
2022-02-24 22:36:31 -0300

    adicionando ponte e reta

--- geometria/Ponto.h
+++ geometria/Ponto.h
@@ -3,6 +3,10 @@ struct Ponto{
     T x, y;
     Ponto(T vx = 0, T vy = 0): x(vx), y(vy) {};
 
+    static double dist (const Ponto<T> &p, const Ponto<T> &q ){
+        return hypot(p.x-q.x, p.x - p.y);
+    }
+
     bool operator==(const Ponto<T> &p) const {
     return equals(x, p.x) and equals(y, p.y);
     }
