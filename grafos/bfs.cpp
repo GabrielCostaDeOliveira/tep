f7eba2f8dca450d0dbe0b67869c9cf3a6ad135de
Gabriel <gabriel.costa513@gmail.com>
2022-03-26 18:12:53 -0300

    adicionando vector de distâncias na bfs

--- grafos/bfs.cpp
+++ grafos/bfs.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+
+const int MAX = 13;
+
 vector<vector<int>> adj {{},
 												{2, 3},      //1
 												{4, 5},      //2
@@ -16,16 +19,16 @@ vector<vector<int>> adj {{},
 												{},          //12
 												{}};         //13
 
-bitset<14> colocado_na_lista;
+bitset<MAX+1> colocado_na_lista;
 
-void bfs(int u){
+void bfs1(int u){
 	queue<int> q;
 	q.push(u);
 
 	while (not q.empty()){
 		int u = q.front(); q.pop();
 
-		cout << u << "- ";
+		cout << u << " ";
 		for(int v : adj[u])
 			if (not colocado_na_lista[v]){
 				colocado_na_lista[v] = true; 
@@ -33,6 +36,50 @@ void bfs(int u){
 			}
 	}
 }
-int main(){
-	bfs(1);
+
+//com as distancias am arestas
+
+const int oo = 1e9 +10;
+
+vector<int> bfs2(int n){
+	vector<int> dist(MAX + 1, oo);
+
+	queue<int> q;
+
+	q.push(n);
+	dist[n] = 0;
+
+	while ( not q.empty() ){
+		auto u = q.front();
+		q.pop();
+
+		//visitamos
+		cout << u << " ";
+		//
+
+		for (auto v : adj[u])
+			if ( dist[v] == oo ){
+				dist[v] = dist[u] + 1;
+				q.push(v);
+			}
+
+	}
+
+
+	return dist;
+
 }
+
+
+int main(){
+	bfs1(1);
+	cout << endl;
+	auto dist =bfs2(1);
+	cout << endl;
+
+
+
+	for (int i = 1; i <= MAX; i++)
+		cout << dist[i] << " ";
+	cout << endl;
+	}
