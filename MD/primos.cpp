93471758ef2f791a9bb42f6ff3b67b1924819355
gabriel costa <gabriel.costa513@gmail.com>
2023-06-04 10:23:38 -0300

    Updade prime file

--- MD/primos.cpp
+++ MD/primos.cpp
@@ -1,138 +1,68 @@
 #include <bits/stdc++.h>
 #define MAX 100
 
+using ll = long long;
 
 using namespace std;
 
-// is_prime é o(n), muito simples e muito custosa;
+bool isPrime(ll x){
 
-bool is_prime (int n){
+  if (x == 1)
+    return false;
+
+  if (x == 2)
+    return true;
+
+  for ( ll i = 2; i*i <= x  ;i++){
+
+    if ( x % i == 0 ){
+      return false;
+    }
+  }
 
 
-	if (n < 2)
-		return false;
-
-	for (int i = 2; i < n; i++)
-		if (n % i == 0)
-			return true;
-
-
-	return false;
-}
-
-//is_prime2 reduz o número de operação, mas não reduz a complexidade
-
-bool is_prime2 (int n){
-
-	if (n < 2)
-		return false;
-
-	if (n == 2)
-		return true;
-
-	if (n % 2 == 0)
-		return false;
-
-	for (int i = 3; i < n; i+=2) // apenas numeros impares chegam aqui 
-		if (n % i == 0)
-			return false;
-
-	return true;
+  return true;
 
 }
 
-//usando o fato de que d < sqrt(n), diminuimos a complexidade para o(sqrt(n))
+ll num_div(ll n){
 
-bool is_prime3 (int n){
-
-// is_prime o (sqrt(n))
-// observe que não foi usado a função sqrt, assim a aaritimérica ficou restrita ao numeros inteiros evitando erros de arrendodamento
-
-	if (n < 2)
-		return false;
-
-	if (n == 2)
-		return true;
-
-	if (n % 2 == 0)
-		return false;
-
-	for (int i = 3; i * i <= n; i+=2) // apenas numeros impares chegam aqui 
-		if (n % i == 0)
-			return false;
-
-	return true;
-
-}
+  ll res = 1;
 
 
-vector<int> primes (int n){
+  for ( int i = 2; i*i <=n ;i++){
 
-	vector<int> ps;
+    ll p = 0;
+    while (n % i == 0){
+      p++;
+      n/= i;
+    }
 
-	for (int i = 2; i <= n; i++)
-		if (is_prime3(i))
-			ps.push_back(i);
+   res *= (p + 1);
+    
+  }
 
-	return ps;
-}
 
-//compolexidade o(n*log(n))
-vector<int> primes2 (int n){
-	vector<int> ps;
-	bitset<MAX> sieve;
-	sieve.set();
-	sieve[1] = false;
+  if (n != 1)
+    res *= 2;
 
-	for (int i = 2; i <= n; i++){
-		if (sieve[i]){
-			ps.push_back(i);
 
-			for (int j = 2*i; j <= n; j+=i)
-				sieve[j] = false;
-		}
-	}
-
-	return ps;
+  return res;
 
 }
 
-// não diminui a complexidade, mas reduz o numeor de instruçãoes
-//
-vector<int> primes3 (int n){
-	bitset<MAX> sieve;
-	vector<int> ps {2};
-	sieve.set();
+vector<bool> crivo(int x) {
+    vector<bool> isPrime(x + 1, true);
+    isPrime[0] = isPrime[1] = false;
 
-	for (int i = 3; i <= n; i += 2) {
-		if (sieve[i]){
-			ps.push_back(i);
+    for (int i = 2; i * i <= x; i++) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= x; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
 
-			for (int j = 2*i; j <= n; j += i)
-				sieve[j] = false;
-		}
-	}
-	return ps;
+    return isPrime;
 }
 
-vector<long long> primes4(long long n){
-	bitset<MAX> sieve;
-	vector<long long> ps {2};
-	sieve.set();
-
-	for (long long i = 3; i <= n; i += 2){
-		if (sieve[i]){
-			ps.push_back(i);
-
-			for (long long j = i*i; j <= n; j += 2*i)
-				sieve[j] = false;
-		}
-	}
-
-	return ps;
-}
-
-
-int main(){
-
-}
