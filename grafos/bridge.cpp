712d9d202ffa79c08ac558ca74911a6a1ca4ac3a
Gabriel Costa de Oliveira <gabriel.costa513@gmail.com>
2023-10-29 23:08:43 -0300

    improve bridge

--- grafos/bridge.cpp
+++ grafos/bridge.cpp
@@ -5,27 +5,23 @@
 using namespace std;
 
 
-void dfs_bridge(int v, int p, vector<pair<int, int>> &bridge,  const vector<vector<int>> &adj, vector<bool> &visited, vector<int> &tin, vector<int> &low,  int& timer){
-
-  visited[v] = true;
+void dfs_bridge(int v, int p, vector<pair<int, int>> &bridge,  const vector<vector<int>> &adj, vector<int> &tin, vector<int> &low,  int& timer){
 
   tin[v] = low[v] = timer++;
 
   for (int to : adj[v]){
-    if (to == p)
-      continue;
 
-    if (visited[to])
-      low[v] = min(low[v], low[to]);
-    else {
-      dfs_bridge(to, v, bridge,adj, visited, tin, low, timer);
+    if (not tin[to]){
+      dfs_bridge(to, v, bridge,adj, tin, low, timer);
 
       low[v] = min(low[v], low[to]);
 
       if ( low[to] > tin[v])
         bridge.push_back({to, v});
-      
-    }
+
+    } else if ( to != p) 
+
+      low[v] = min(low[v], low[to]);
 
   }
 
@@ -35,16 +31,15 @@ void dfs_bridge(int v, int p, vector<pair<int, int>> &bridge,  const vector<vect
 
 vector<pair<int, int>> find_bridge(const vector<vector<int>> &adj){
 
-
-  vector<bool> visited(adj.size(), false);
   vector<int> tin(adj.size(), 0), low(adj.size(), 0);
   int timer = 0;
 
   vector<pair<int, int>> bridge;
 
-  dfs_bridge(0, -1, bridge, adj, visited, tin, low, timer);
+  dfs_bridge(0, -1, bridge, adj, tin, low, timer);
   
   return bridge;
 
 }
+
 
