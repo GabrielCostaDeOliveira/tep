2d03fff5797667e8fc421834316286076f59fec5
Gabriel Costa de Oliveira <gabriel.costa513@gmail.com>
2023-11-17 07:38:08 -0300

    update dijkstra

--- grafos/dijkstra.cpp
+++ grafos/dijkstra.cpp
@@ -1,90 +1,35 @@
 #include <bits/stdc++.h>
+#include <queue>
 
 using namespace std;
 
-const int oo {1000000010};
-const int MAX_V = 6;
+vector<int> dijkstra(int s, const vector<vector<pair<int, int>>> &adj){
 
-using ii = pair<int, int>;
+  const long long oo = 1e9;
+  vector<bool> visited(adj.size(), false);
+  vector<int> dist(adj.size(), oo);
+  dist[s] = 0;
 
-vector<vector<ii>> adj {
+  priority_queue<pair<int,int>, vector<pair<int, int>>, greater<pair<int,int>>> pq;
+  pq.emplace(0, s);
 
-{},
-{ {2, 9}, {3, 7}, {4, 4}, {5, 2} },  //A
-{ {1, 9}, {3, 1}, {6, 3} },          //B
-{ {1, 7}, {2, 1}, {4, 2} },          //C
-{ {1, 4}, {3, 2}, {5, 1} },          //D
-{ {1, 2}, {4, 1}, {6, 11} },         //E
-{ {2, 3}, {5, 11} }                  //F
-																		 //
-};
+  while (not pq.empty()){
 
-vector<int> dijkstra1(int s, int n){
-	vector<int> dist(n+1, oo);
-	dist[s] = 0;
+    auto [d, u] = pq.top();
+    pq.pop();
 
-	priority_queue<ii, vector<ii>, greater<ii>> p;
-	p.push(ii(s, 0));
+    if (visited[u])
+      continue;
 
-	bitset<MAX_V + 1> visited;
+    visited[u] = true;
 
-	while ( not p.empty() ){
+    for (auto [w, v] : adj[u])
+      if ( dist[u] + w < dist[v]){
+        dist[v] = dist[u] + w;
+        pq.emplace(dist[v], v);
+      }
+  }
 
-		auto [u, d] = p.top();
-		p.pop();
-
-		if ( visited[u] )
-			continue;
-
-		visited[u] = true;
-
-		for (auto [v, w] : adj[u])
-			if ( dist[v] > w + d ){
-				dist[v] = d + w;
-				p.emplace(dist[v], v);
-			}
-	}
-
-	return dist;
+  return dist;
 }
 
-vector<int> dijkstra2(int s, int n){
-
-	vector<int> dist(n+1,oo);
-	dist[1] = 0;
-
-	set<ii> U;
-	U.emplace(0,s);
-
-	while (not U.empty()){
-		auto [d, u] = *U.begin();
-		U.erase(U.begin());
-
-		//operação de relaxamanto 
-		
-		//para todas as adjacẽncias de u
-		for (auto [v, w] : adj[u])
-			if (dist[v]> d + w){
-
-				//se já tem um distancia para v apague
-				if(U.count(make_pair(dist[v],v)))
-					U.erase(make_pair(dist[v],v));
-
-				dist[v] = d + w;
-				U.emplace(dist[v],v);
-			}
-	}
-
-	return dist;
-}
-
-int main(){
-	for  ( auto d : dijkstra1(1, MAX_V) )
-		cout << d << " ";
-	cout << endl;
-
-	for  ( auto d : dijkstra2(1, MAX_V) )
-		cout << d << " ";
-	cout << endl;
-		
-}
