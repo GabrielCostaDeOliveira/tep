96e950dc7ecf9bbf09201024b2b6b7b7aa9b93ec
Gabriel Costa de Oliveira <gabriel.costa513@gmail.com>
2023-11-17 08:39:57 -0300

    add totient function

---
+++ MD/totient_function.cpp
@@ -0,0 +1,40 @@
+// Euler's totient function or Euler's phi function is  is the number of integers k in the range 1 ≤ k ≤ n for which the greatest common divisor gcd(n, k) is equal to 1
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+long long fast_pow(long long a, long long b) {
+  if ( b == 0)
+    return 1;
+
+  long long u = fast_pow(a, b/2);
+  return b & 1 ? a * u * u :  u * u;
+}
+
+
+long long phi(long long x){
+  long long res = 1;
+
+  for ( long long i = 2; i * i  <= x; i++ ){
+
+    int qnt = 0;
+    while ( x % i == 0 ){
+      qnt++;
+      x/= i;
+    }
+
+    if (qnt != 0)
+      res *= (i - 1)  * fast_pow(i, qnt-1);
+
+  }
+
+  if (x != 1)
+    res *= (x - 1);
+
+
+  return res;
+
+}
+
