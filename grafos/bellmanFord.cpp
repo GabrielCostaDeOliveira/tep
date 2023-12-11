2b755bbc5b5e6e7c3d1d036d2265ddb5abb3b506
Gabriel Costa de Oliveira <gabriel.costa513@gmail.com>
2023-12-11 14:34:05 -0300

    update

--- grafos/bellmanFord.cpp
+++ grafos/bellmanFord.cpp
@@ -1,18 +1,15 @@
 #include <bits/stdc++.h>
 
-using namespace std;
-
-using edge = tuple<int, int, int>;
 
 // cominho mínimos de fonte únicas
 // É capaz de processar arestas negativas
 // identifica ciclos negativos
 // O(V*E)
 
-vector<int> bellmanfor(int s/*start*/, int N /*n de vertice*/, const vector<edge> & edges /*lista de arestas*/ ){
+std::vector<int> bellmanfor(int s/*start*/, int N /*n de vertice*/, const std::vector<std::tuple<int, int, int>> & edges /*lista de arestas*/ ){
 	const int oo {1000000010};
 
-	vector<int> dist(N + 1, oo);
+  std::vector<int> dist(N + 1, oo);
 	dist[s] = 0;
 
 	for (int i = 1; i <= N - 1; i++)
@@ -21,27 +18,5 @@ vector<int> bellmanfor(int s/*start*/, int N /*n de vertice*/, const vector<edge
 				dist[v] = dist[u] +w; // relaxamanro 
 
 	return dist;
-}
-
-
-int main(){
-	int u, v, w;
-
-	vector<edge> edges {{0, 0, 0}};
-
-	//leitua da lista de arestas
-	 do{
-		cin >> u >> v >> w;
-		edges.emplace_back(u,v,w);
-	}while ( u != 0 or v != 0 or w != 0 );
-	 
-
-	//removendo o primeiro elemento que é um lixo e imprimeindo 
-	//as distáncias 
-  auto dist = bellmanfor(1, 6,edges);
-	dist.erase(dist.begin()); 
-	for (int d: dist)
-	 cout << d  << " ";
-	cout << endl;
 }
 
--- grafos/floydWarshall.cpp
+++ grafos/floydWarshall.cpp
@@ -3,20 +3,10 @@
 using namespace std;
 
 const int oo = 1e9;
-vector<vector<pair<int, int>>> adj{
-	{},
-	{ {}, {2, 9}, {3, 7}, {4, 4}, {5, 2}}, 
-	{	{}, {1, 9}, {3, 1}, {6, 3}}, 
-	{ {}, {1, 7}, {2, 1}, {4, 2} }, 
-	{ {}, {1, 4}, {3, 2}, {5, 1} },
-	{ {}, {1, 2}, {4, 1}, {6, 11}}, 
-	{ {}, {2, 3}, {5, 11} }
 
-};
+std::vector<std::vector<int>> floyd_warshall(int N, std::vector<std::vector<pair<int, int>>> adj){
 
-vector<vector<int>> floyd_warshall(int N){
-
-	vector<vector<int>> dist(N+1, vector<int>(N+1, oo));
+  std::vector<vector<int>> dist(N+1, std::vector<int>(N+1, oo));
 
 	for ( int u = 1; u <= N; u++ )
 		dist[u][u] = 0;
@@ -35,12 +25,3 @@ vector<vector<int>> floyd_warshall(int N){
 	return dist;
 }
 
-int main(){
-
-	auto dist = floyd_warshall(6);
-
-	for (int u = 1; u <= 6; u++)
-		for ( int v = 1; v <= 6; v++ )
-			cout << dist[u][v] << (v == 6 ? "\n" : " ");
-
-}
