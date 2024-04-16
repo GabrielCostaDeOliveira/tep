ad495adc41aa04b281d04c52b871232359f6b600
Gabriel Costa de Oliveira <gabriel.costa513@gmail.com>
2024-04-16 12:04:06 -0300

    add dir

---
+++ utils/dir.cpp
@@ -0,0 +1,20 @@
+
+#include<bits/stdc++.h>
+
+std::vector<std::pair<int, int>> dir8{
+  { 1,  0},   
+  { 1,  1},   
+  { 0,  1},   
+  {-1,  1},  
+  {-1,  0},  
+  {-1, -1}, 
+  { 0, -1},  
+  { 1, -1}   
+};
+
+std::vector<std::pair<int, int>> dir4{
+  {1, 0},   
+  {0, 1},   
+  {-1, 0},  
+  {0, -1}   
+};
