984a7844aad24c7968506202b65cb182f3614c15
Gabriel Costa de Oliveira <gabriel.costa513@gmail.com>
2024-05-14 15:14:36 -0300

    add test to segtree

--- unit_test/data-structures/SegTree.cpp
+++ unit_test/data-structures/SegTree.cpp
@@ -35,6 +35,31 @@ TEST ( SegTree, QUERY_UPDATE_MIN){
 
 }
 
+TEST ( SegTree, QUERY_UPDATE_MIN_PAIR){
+  const int oo = 1e9;
+
+                  // 0  1  2   3   4   5   6   7  8  9
+  std::vector<int> v{9, 2, 1, 12, 23, 23, 23, 15, 9, 1};
+
+  SegTree<std::pair<int, int>> segtree(v.size(), {oo, oo}, [](std::pair<int, int> a, std::pair<int, int> b){return std::min(a, b);});
+
+  for ( size_t i = 0; i < v.size(); i++)
+    segtree.update(i, {v[i], i});
+
+
+  ASSERT_EQ(segtree.query(0, 0), std::make_pair(9 , 0));
+  ASSERT_EQ(segtree.query(1, 1), std::make_pair(2 , 1));
+  ASSERT_EQ(segtree.query(2, 2), std::make_pair(1 , 2));
+  ASSERT_EQ(segtree.query(3, 3), std::make_pair(12, 3));
+  ASSERT_EQ(segtree.query(4, 4), std::make_pair(23, 4));
+
+  ASSERT_EQ(segtree.query(3, 4), std::make_pair(12, 3));
+  ASSERT_EQ(segtree.query(3, 8), std::make_pair(9, 8));
+  ASSERT_EQ(segtree.query(4, 7), std::make_pair(15, 7));
+
+
+}
+
 
 TEST(SegTree, QUERY_UPDATE_XOR ){
 
@@ -46,6 +71,9 @@ TEST(SegTree, QUERY_UPDATE_XOR ){
   for ( auto v : {3, 2, 4, 5, 1, 1, 5, 3})
     st.update(i++, v);
 
+  ASSERT_EQ(st.query(0, 0), 3);
+  ASSERT_EQ(st.query(1, 1), 2);
+  ASSERT_EQ(st.query(3, 3), 5);
 
   ASSERT_EQ(st.query(1, 3), 3);
   ASSERT_EQ(st.query(4, 5), 0);
