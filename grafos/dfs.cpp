d635a07fc47a5c49441eb1926e0dcc5f695ffd09
Gabriel Costa de Oliveira <gabriel.costa513@gmail.com>
2024-05-14 15:13:37 -0300

    update dfs

--- grafos/dfs.cpp
+++ grafos/dfs.cpp
@@ -2,49 +2,20 @@
 
 using namespace std;
 
-vector<vector<int>> adj {
-	{},
-	{2, 3},      //1
-	{4, 5},      //2
-	{6},         //3
-	{},          //4
-	{2,6,8, 9},  //5
-	{3,5,7, 10}, //6
-	{},          //7
-	{11},        //8
-	{5,10},      //9
-	{9, 6},      //10
-	{12, 13},    //11
-	{},          //12
-	{}           //13
 
-};         
-bitset<13+1> visitado;
+std::vector<bool> visited;
+vector<vector<int>> adj;
 
-void dfs(int u){
+auto dfs = [](auto &&dfs, int u){
 
-	if (visitado[u])
-		return;
-	
-	
-	cout << u <<" -" ;
+  if (visited[u])
+    return;
 
+  visited[u] = true;
 
-	visitado[u] = true;
-	 
-	for (auto v: adj[u])
-				dfs(v);
-		
-}
+  for ( auto v : adj[u])
+    dfs(dfs, v);
 
-
-
-
-
-int main(){
-	dfs(1);
-
-}
-
+};
 
 
