f4e10085357adff2891da2d9bf322f8bd5f79b5d
Gabriel Costa de Oliveira <gabriel.costa513@gmail.com>
2025-02-23 11:48:54 -0300

    fix

--- MD/fast_pow.cpp
+++ MD/fast_pow.cpp
@@ -1,10 +1,16 @@
 
 #include <bits/stdc++.h>
 
+
 template<typename T>
 T fast_pow(const T a, const T b, const T MOD) {
-    if (b == 0) 
-        return (T)1;
-    T p = fast_pow(a%MOD, b/2, MOD) % MOD;
-    return b & 1 ? (a*((p*p)%MOD))%MOD : (p*p)%MOD;
+
+  if (b == 0)
+    return 1;
+
+  long long x = fast_pow( (T) a, (T) b / 2ll);
+
+  x = (x * x) % MOD;
+
+  return b & 1 ? (a * x) % MOD : x;
 }
--- MD/sieve.cpp
+++ MD/sieve.cpp
@@ -5,7 +5,7 @@ std::vector<long long> sieve(long long n){
 
   std::vector<bool> is_prime(n, true);
 
-  for (long long i = 2; i < n; i++){
+  for (long long i = 2; i*i < n; i++){
     if (is_prime[i])
       for (long long j = i*i; j < n; j+=i )
         is_prime[j] = false;
