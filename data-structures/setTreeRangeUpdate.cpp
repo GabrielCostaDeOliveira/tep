947df5be2647e3a8d0707136f28d3242656481ec
gabriel <gabriel.costa513@gmail.com>
2025-03-07 14:24:41 -0300

    update set tree range update

--- data-structures/setTreeRangeUpdate.cpp
+++ data-structures/setTreeRangeUpdate.cpp
@@ -4,50 +4,49 @@ using namespace std;
 
 using ll = long long;
 
+template <typename T>
 struct SegTree {
 
-  vector<ll> t;
-
   ll n;
+  vector<T> t;
 
-  SegTree(ll n) : t(4 * n, 0), n(n) {}
+  SegTree(size_t n_p) : n(n_p),  t(4 * n_p, 0) {}
 
-  void update(ll l, ll r, ll value) {
+  void update(size_t l, size_t r, const T value) {
 
-    auto _update = [&](auto &&self, ll l, ll r, ll tl, ll tr, ll p) -> void {
+    auto _update = [this, &value](auto &&self, size_t l, size_t r, size_t tl, size_t tr, size_t p) -> void {
       if (l > r)
         return;
 
-      if (l == tl and r == tr)
+      if (l == tl and r == tr) {
         t[p] += value;
-      else {
-        ll tm = (tl + tr) / 2;
-        self(self, l, min(tm, r), tl, tm, 2 * p);
-        self(self, max(l, tm + 1), r, tm + 1, tr, 2 * p + 1);
+        return;
       }
+
+      size_t tm = midpoint(tl, tr);
+
+      self(self, l, min(tm, r), tl, tm, 2 * p);
+      self(self, max(l, tm + 1), r, tm + 1, tr, 2 * p + 1);
     };
 
-    _update(_update, l, r, 1, n, 1);
+    _update(_update, l, r, 0, n-1, 1);
   }
 
-  ll query(ll i) {
+  T query(const size_t i) {
 
-    auto _query = [&](auto &&self, ll i, ll tl, ll tr, ll p) -> ll {
-
+    auto _query = [this, i](auto &&self, size_t tl, size_t tr, T p) -> ll {
       if (tl == tr)
         return t[p];
 
-      else {
+      size_t tm = midpoint(tl, tr);
 
-        ll tm = midpoint(tl, tr);
-
-        if (i <= tm)
-          return t[p] + self(self, i, tl, tm, 2 * p);
-        else
-          return t[p] + self(self, i, tm + 1, tr, 2 * p + 1);
-      }
+      if (i <= tm)
+        return t[p] + self(self, tl, tm, 2 * p);
+      else
+        return t[p] + self(self, tm + 1, tr, 2 * p + 1);
     };
 
-    return _query(_query, i, 1, n, 1);
+    return _query(_query, 0, n-1, 1);
   }
 };
+
