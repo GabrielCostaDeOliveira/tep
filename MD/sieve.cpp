f98360cdc5a363f9c4b664b91985e1afeabebcf1
gabriel <gabriel.costa513@gmail.com>
2025-03-10 20:37:34 -0300

    update sieve

--- MD/sieve.cpp
+++ MD/sieve.cpp
@@ -1,19 +1,20 @@
 #include <vector>
 
-std::vector<long long> sieve(long long n){
+std::vector<long long> sieve(long long n) {
 
-  std::vector<bool> is_prime(n, true);
-  std::vector<long long> prime;
+  std::vector<bool> is_prime(n + 1, true);
+  std::vector<long long> primes;
 
-  for (long long i = 2; i*i <= n; i++)
-    if (is_prime[i] and i * i <= n){
-
-      prime.push_back(i);
-
-      for (long long j = i*i; j < n; j+=i )
+  for (long long i = 2; i*i <= n; i++) 
+    if (is_prime[i] and i*i <= n)
+      for (long long j = i*i; j <= n; j+=i )
         is_prime[j] = false;
-    }
 
-  return prime;
+  for (long long i = 2; i <= n; i++) 
+    if (is_prime[i])
+      primes.push_back(i);
+
+  return primes;
 
 }
+
