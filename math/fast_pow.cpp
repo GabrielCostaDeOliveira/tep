f0bd24f5ff6b2ca60b7dccffa0b2b9713fa5ed84
gabriel <gabriel.costa513@gmail.com>
2025-03-20 17:35:59 -0300

    add interative version to fast pow

--- math/fast_pow.cpp
+++ math/fast_pow.cpp
@@ -1,6 +1,4 @@
-
 #include <bits/stdc++.h>
-
 
 template<typename T>
 T fast_pow(const T a, const T b, const T MOD) {
@@ -13,4 +11,16 @@ T fast_pow(const T a, const T b, const T MOD) {
   x = (x * x) % MOD;
 
   return b & 1 ? (a * x) % MOD : x;
+}
+
+template<typename T>
+T fast_pow2(const T a,const T b, const T MOD) {
+
+  T res = 1;
+
+  for (; b; b>>=1, a = (a*a)%MOD)
+    if (b&1)
+      res = (res * a)%MOD;
+
+  return res;
 }
