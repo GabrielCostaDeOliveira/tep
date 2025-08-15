734e66f9e520b69ec3a62e90e6a577cd10b80372
gabriel <gabriel.costa513@gmail.com>
2025-08-15 20:08:52 -0300

    refactor code

--- graph/bellmanFord.cpp
+++ graph/bellmanFord.cpp
@@ -1,22 +1,15 @@
 #include <bits/stdc++.h>
 
-
-// cominho mínimos de fonte únicas
-// É capaz de processar arestas negativas
-// identifica ciclos negativos
-// O(V*E)
-
-std::vector<int> bellmanfor(int s/*start*/, int N /*n de vertice*/, const std::vector<std::tuple<int, int, int>> & edges /*lista de arestas*/ ){
-	const int oo {1000000010};
+std::vector<int> bellmanfor( int s, int N, const std::vector<std::tuple<int, int, int>> &edges ) {
+  const int oo{1000000010};
 
   std::vector<int> dist(N + 1, oo);
-	dist[s] = 0;
+  dist[s] = 0;
 
-	for (int i = 1; i <= N - 1; i++)
-		for (auto [u, v, w] : edges)
-			if (dist[u] < oo and dist[v] > dist[u] + w )
-				dist[v] = dist[u] +w; // relaxamanro 
+  for (int i = 1; i <= N - 1; i++)
+    for (auto [u, v, w] : edges)
+      if (dist[u] < oo and dist[v] > dist[u] + w)
+        dist[v] = dist[u] + w; // relaxamanro
 
-	return dist;
+  return dist;
 }
-
